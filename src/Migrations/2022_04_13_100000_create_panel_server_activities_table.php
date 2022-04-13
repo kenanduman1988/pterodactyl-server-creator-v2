cb98d4f2f8b686c2d4a7342d42c5dd5155848839
Branimir369 <branimir@bangergames.com>
2022-04-13 16:24:58 +0200

    Added Panel Server Activity table, small fix

--- src/Migrations/2022_04_13_100000_create_panel_server_activities_table.php
+++ src/Migrations/2022_04_13_100000_create_panel_server_activities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class PanelServerActivities extends Migration
+class CreatePanelServerActivitiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class PanelServerActivities extends Migration
      */
     public function up()
     {
-        Schema::table('panel_server_activities', function (Blueprint $table) {
+        Schema::create('panel_server_activities', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->unsignedBigInteger('panel_server_id');
